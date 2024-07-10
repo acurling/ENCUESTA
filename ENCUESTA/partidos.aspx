8448a8c9c25b13e484cb33354736f4d523c0f6a4
acurling <acurling@hotmail.com>
2024-07-09 21:06:55 -0600

    Agregar archivos de proyecto.

---
+++ ENCUESTA/partidos.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="partidos.aspx.cs" Inherits="ENCUESTA.partidos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="css/menu.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>CATALOGO DE PARTIDOS POLITICOS</h1>
+        </div>
+        <div>
+            <br />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ENCUESTA/partidos.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ENCUESTA
+{
+    public partial class partidos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LlenarGrid();
+        }
+
+        protected void LlenarGrid()
+        {
+            // almacena en la variable constr la ruta para la conexion al servidor
+            string constr = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT *  FROM PARTIDOSPOLITICOS"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind(); // refrescar
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
